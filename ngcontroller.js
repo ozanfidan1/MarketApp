cf9dd677f4ba6b7256ddfe3e57513ffa869f1bbb
ozanfidan1 <124314444+ozanfidan1@users.noreply.github.com>
2023-03-20 16:55:16 +0300

    Add files via upload

--- ngcontroller.js
+++ ngcontroller.js
@@ -4,11 +4,4 @@ app.controller('urunislemleri', function ($scope,$http) {
         .then(function (response) {
             $scope.products = response.data;
         });
-    $scope.urunsil = function (silinecekID)
-    {
-        $http.get("https://localhost:44385/api/Product/UrunSil/", { params: { id: silinecekID } })
-            .then(function (response) {
-                $scope.products = response.data;
-            });
-    }
 });
