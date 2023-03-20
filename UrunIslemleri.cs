1c598c0ee3c69a63a3d79b2234328a361cd5a284
ozanfidan1 <124314444+ozanfidan1@users.noreply.github.com>
2023-03-20 13:54:52 +0300

    Add files via upload

---
+++ UrunIslemleri.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MarketApplication
+{
+    public class UrunIslemleri
+    {
+        marketEntities Entity = new marketEntities();
+
+        public List<UrunTip> TumUrunleriGetir()
+        {
+            return Entity.Products.Select(p => new UrunTip()
+            {
+                id = p.id,
+                SubcategoryID = p.SubcategoryID,
+                Name = p.Name,
+                Description = p.Description,
+                Price = p.Price,
+                Quantity = p.Quantity,
+                Unit = p.Unit
+            }).ToList();
+        }
+
+        public List<UrunTip> KategoriyeGoreUrunleriGetir(Guid SubcategoryID)
+        {
+            return Entity.Products.Where(p=>p.SubcategoryID==SubcategoryID).Select(p => new UrunTip()
+            {
+                id = p.id,
+                SubcategoryID = p.SubcategoryID,
+                Name = p.Name,
+                Description = p.Description,
+                Price = p.Price,
+                Quantity = p.Quantity,
+                Unit = p.Unit
+            }).ToList();
+        }
+
+        public List<UrunTip> UrunSil(Guid id)
+        {
+            Products deleted = Entity.Products.Find(id);
+            Entity.Products.Remove(deleted);
+            Entity.SaveChanges();
+            return TumUrunleriGetir();
+        }
+    }
+
+    public class UrunTip
+    {
+        public System.Guid id { get; set; }
+        public Nullable<System.Guid> SubcategoryID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public double Quantity { get; set; }
+        public string Unit { get; set; }
+    }
+}
---
+++ ngcontroller.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module('market', []);
+app.controller('urunislemleri', function ($scope,$http) {
+    $http.get("https://localhost:44385/api/Product/TumUrunleriGetir")
+        .then(function (response) {
+            $scope.products = response.data;
+        });
+    $scope.urunsil = function (silinecekID)
+    {
+        $http.get("https://localhost:44385/api/Product/UrunSil/", { params: { id: silinecekID } })
+            .then(function (response) {
+                $scope.products = response.data;
+            });
+    }
+});
